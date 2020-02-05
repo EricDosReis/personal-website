c431a4d562138d7f38330c4b23dbf825862e357a
ericsantos <eric@exactaworks.com.br>
2020-02-05 13:05:41 -0300

    cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `EricDosReis profile`,
+    description: `Eric dos Reis personal web site`,
+    author: `EricDosReis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
