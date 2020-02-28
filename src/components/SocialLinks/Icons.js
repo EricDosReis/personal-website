9456138519c0a948ded608cda4eded8a6d8667ff
ericsantos <eric@exactaworks.com.br>
2020-02-28 17:51:22 -0300

    create Icons component, add styled-icons

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { LinkedIn } from "styled-icons/boxicons-logos/Linkedin"
+import { Youtube } from "styled-icons/boxicons-logos/Youtube"
+import { Instagram } from "styled-icons/boxicons-logos/Inst"
+
+const Icons = {
+  Github,
+  LinkedIn,
+  Youtube,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/EricDosReis/",
+  },
+  {
+    label: "LinkedIn",
+    url: "https://linkedin.com/in/ericdosreis/",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/Skatude/",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/eric_dos_reis/",
+  },
+]
+
+export default links
