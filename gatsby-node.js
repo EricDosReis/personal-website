87343d4a3d96b5bc6b888e4d77c1b1cc2b5845db
ericsantos <eric@exactaworks.com.br>
2020-03-09 13:59:57 -0300

    create post template page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
 
 const POST_DATE_LENGTH = 12
 
@@ -18,4 +19,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${slug.slice(POST_DATE_LENGTH)}`,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
--- posts/2019-08-15-lorem-ipsum.md
+++ posts/2019-08-15-lorem-ipsum.md
@@ -6,8 +6,6 @@ category: JS
 background: "#7aab13"
 ---
 
-# Lorem ipsum
-
 Proin suscipit luctus orci placerat fringilla. Donec hendrerit laoreet risus eget adipiscing. Suspendisse in urna ligula, a volutpat mauris. Sed enim mi, [adipiscing](http://google.com) eu pulvinar vel, sodales vitae dui. :thumbsup: :smile: :sparkler:
 
 ```javascript
